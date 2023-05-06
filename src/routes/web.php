ad5e133e92a5850f9c05a270fcb7540d23e6aa16
Jun Iwamura <soipon0@gmail.com>
2023-05-06 16:08:15 +0900

    create top page

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.home.index');
 });
 
 Route::get('/dashboard', function () {
