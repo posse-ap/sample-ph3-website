343c616450fb5ed5d748027c5e4c5e22256ab324
Jun Iwamura <soipon0@gmail.com>
2023-05-06 13:13:33 +0900

    change: DatabaseSeeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create(['name' => 'admin', 'email' => 'test+1@posse-ap.com', 'password' => bcrypt('password')]);
         $quizzes = $this->listOfQuiz();
         foreach ($quizzes as $quiz) {
             $question = Question::create(['content' => $quiz['question'], 'supplement' => '', 'image' => '']);
