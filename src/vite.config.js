1a55791826dcd84855c78405738e3aa1eee3ce34
Jun Iwamura <soipon0@gmail.com>
2023-05-10 16:35:34 +0900

    必要な静的要素を追加

--- src/vite.config.js
+++ src/vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/common.js',
+                'resources/js/quiz.js',
                 'resources/scss/common.scss',
             ],
             refresh: true,
