c24fe5681eb258437b5e536eb8c2526770f3bb36
Jun Iwamura <soipon0@gmail.com>
2023-05-10 16:32:44 +0900

    クイズの遷移先を変更

--- src/resources/views/components/user/common/header.blade.php
+++ src/resources/views/components/user/common/header.blade.php
@@ -8,7 +8,7 @@
                     <a href="/" class="p-header__nav__item__link">POSSEとは</a>
                 </li>
                 <li class="p-header__nav__item">
-                    <a href="/quiz/" class="p-header__nav__item__link">クイズ</a>
+                    <a href="/quizzes" class="p-header__nav__item__link">クイズ</a>
                 </li>
             </ul>
         </nav>
